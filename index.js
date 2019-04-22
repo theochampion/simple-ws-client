78a957cedd76d8c791719df4064f0a4a5fd4b977
theochampion <contact.theochampion@gmail.com>
2019-04-22 13:59:43 +1000

    :construction: Interactive selection of conversation

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const io = require("socket.io-client");
 const fetch = require("node-fetch");
 const minimist = require("minimist");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	prompt: "SWSC> "
+});
 
 /** Constants */
 const API_ROOT_URL = "http://localhost:3030";
@@ -75,6 +82,12 @@ const CONVERSATION_URL = `${API_ROOT_URL}/conversation`;
 // 	}
 // );
 
+ask = questionText => {
+	return new Promise((resolve, reject) => {
+		rl.question(questionText, input => resolve(input));
+	});
+};
+
 const authenticate = async (email, password) => {
 	try {
 		const res = await fetch(LOGIN_URL, {
@@ -86,10 +99,9 @@ const authenticate = async (email, password) => {
 		const cookies = res.headers.get("set-cookie");
 		const user = await res.json();
 		console.log(
-			`\x1b[32mLogged in as \x1b[4m${user.name} ${
-				user.lastname
-			}. \x1b[0m\x1b[32mFetching conversations...\x1b[0m`
+			`\x1b[32mLogged in as \x1b[4m${user.name} ${user.lastname}\x1b\x1b[0m`
 		);
+		rl.setPrompt(`${user.name} ${user.lastname}`);
 		return cookies;
 	} catch (err) {
 		console.error(err);
@@ -97,12 +109,41 @@ const authenticate = async (email, password) => {
 	}
 };
 
+const selectConversation = async session_cookie => {
+	try {
+		const res = await fetch(CONVERSATION_URL, {
+			method: "get",
+			headers: { Cookie: session_cookie }
+		});
+		if (!res.ok) return null;
+		const body = await res.json();
+		if (body.conversations.length == 0) return null;
+		console.log(`\x1b[35mSelect a conversation to join:\x1b[0m`);
+		const conversation_ids = body.conversations.map((conversation, idx) => {
+			console.log(`[${idx}] "${conversation._id}"`);
+			return conversation._id;
+		});
+
+		const conversation_id = await ask(
+			"Select a conversation leave blank to exit : "
+		);
+		return conversation_ids.includes(conversation_id) ? conversation_id : null;
+	} catch (err) {
+		console.error(err);
+		return null;
+	}
+};
+
 const main = async (email, password) => {
-	const cookies = await authenticate(email, password);
-	if (cookies == null)
+	const session_cookie = await authenticate(email, password);
+	if (session_cookie == null)
 		return console.error(
 			`\x1b[33mConnection failed, check your credentials. Aborting.\x1b[0m`
 		);
+
+	const conversation_id = await selectConversation(session_cookie);
+	rl.setPrompt(`[${conversation_id}]>`);
+	rl.prompt();
 };
 /** Parse command line arguments and start script */
 let email, password;
