f5b4dc6bbc66bb85679be3fec22a314a402fe00d
theochampion <contact.theochampion@gmail.com>
2019-04-22 21:21:33 +1000

    :fire: Remove dead code

--- index.js
+++ index.js
@@ -14,57 +14,6 @@ const API_ROOT_URL = "http://localhost:3030";
 const LOGIN_URL = `${API_ROOT_URL}/login`;
 const CONVERSATION_URL = `${API_ROOT_URL}/conversation`;
 
-// /*
-//  * Authenticate first, doing a post to some url
-//  * with the credentials for instance
-//  */
-// request.post(
-// 	{
-// 		url: LOGIN_URL,
-// 		form: { mail: email, password: password }
-// 	},
-// 	(err, resp, body) => {
-// 		if (err || resp.statusCode !== 200) {
-// 			console.error(
-// 				`\x1b[35mConnection failed, check your credentials. Aborting.\x1b[0m`
-// 			);
-// 			process.exit(1);
-// 		}
-// 		// Grab auth cookies form response headers
-// 		const cookies = resp.headers["set-cookie"];
-// 		const json_body = JSON.parse(body);
-// 		console.log(
-// 			`\x1b[32mLogged in as \x1b[4m${json_body.name} ${
-// 				json_body.lastname
-// 			}. \x1b[0m\x1b[32mFetching conversations...\x1b[0m`
-// 		);
-// 		request.get(
-// 			{
-// 				url: CONVERSATION_URL,
-// 				headers: {
-// 					Cookie: cookies
-// 				}
-// 			},
-// 			(err, resp, body) => {
-// 				if (err || resp.statusCode !== 200) {
-// 					console.error(
-// 						`\x1b[35mError fetching conversations. Aborting.\x1b[0m`
-// 					);
-// 					process.exit(1);
-// 				}
-// 				const conversations = JSON.parse(body).conversations;
-// 				console.error(`\x1b[35mSelect a conversation to join:\x1b[0m`);
-// 				conversations.forEach((conversation, idx) =>
-// 					console.log(`[${idx}] "${conversation._id}"`)
-// 				);
-// 			}
-// 		);
-
-// 		const convoID = "ekognerknp00ng4lshn0bf40dnfyu4o2nfkdhfw4nfafn3ifnambf";
-
-// 	}
-// );
-
 ask = questionText => {
 	return new Promise((resolve, reject) => {
 		rl.question(questionText, input => resolve(input));
@@ -137,11 +86,16 @@ const connectToConversation = async (user, conversation_id) => {
 			);
 			rl.on("line", line => {
 				socket.emit("message", { sender: user._id, content: line, type: 0 });
-				console.log(`Line from file: ${line}`);
 			});
 		});
+
 		socket.on("new_message", msg => {
-			console.log(`[${msg.sender}] ${msg.content}`);
+			console.log(msg);
+			console.log(
+				`\x1b[${msg.sender == user._id ? "32" : "35"}m <${msg.sender}> ${
+					msg.content
+				}\x1b[0m`
+			);
 		});
 
 		socket.on("disconnect", () => {
